25f5f9338faf68536f311ec6daa3e26b858fdd0f
n.skuz <zux2007@gmail.com>
2013-11-09 23:40:26 +0200

    Added Doctrine and Twig

--- index.php
+++ index.php
@@ -3,13 +3,32 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->get('/', function() use ($app) {
-	return 'Hello World!';
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver' => 'pdo_mysql',
+        'user' => 'root',
+        'password' => 'fuckoff',
+        'dbname' => 'silex',
+        'charset' => 'utf-8',
+    ),
+));
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+$app->before(function () use ($app) {
+    $app['twig']->addGlobal('layout', $app['twig']->loadTemplate('layout.twig'));
 });
 
-$app->get('/asd', function(Request $request) {
-    return ($request);
+$app->get('/', function() use ($app) {
+	return $app['twig']->render('index.twig', array(
+        'title' => 'Main page',
+    ));
 });
+
+$app->get('/add', function() use ($app) {
+    return 'Add news';
+});
+
 
 
 $app['debug'] = true;
